8e60138b2d7232d04a447936fcb718de90788239
Yoshiya Hinosawa <stibium121@gmail.com>
2016-02-21 19:05:24 +0900

    Initial commit

---
+++ src/main/kotlin/org/kt3k/ebean/enhance/EnhanceEbeanPlugin.kt
@@ -0,0 +1,36 @@
+package org.kt3k.ebean.enhance
+
+import org.gradle.api.*
+
+public class EnhanceEbeanPlugin : Plugin<Project> {
+
+  override fun apply(project: Project) {
+
+    val conf = project.getConfigurations().create("enhance")
+    val task = project.task("enhanceEbean")
+
+    task.doLast(object: Action<Task> {
+
+      override fun execute(task: Task) {
+
+        project.ant.invokeMethod("taskdef", mapOf(
+          "name" to "enhanceEbean",
+          "classname" to "com.avaje.ebean.enhance.ant.AntEnhanceTask",
+          "name" to conf.asPath
+        ))
+
+        project.ant.invokeMethod("enhanceEbean", mapOf(
+          "classSource" to project.buildDir.absolutePath + "src/main",
+          "packages" to conf,
+          "transformArgs" to "debug=5"
+        ))
+
+      }
+
+    })
+
+    (project.property("classes") as Task).dependsOn(task)
+
+  }
+
+}
